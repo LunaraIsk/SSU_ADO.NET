fd9da8491f6e2d32a444838979e0eb075be8ad0e
iskalievalunara0 <iskalievalunara0@gmail.com>
2020-10-09 17:18:39 +0300

    Добавьте файлы проекта.

---
+++ 1b.IskalievaLE.WebPl/Controllers/InventoryController.cs
@@ -0,0 +1,28 @@
+﻿using _1b.IskalievaLE.BLL.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _1b.IskalievaLE.Common;
+using DependencyResolver = _1b.IskalievaLE.Common.DependencyResolver;
+
+namespace _1b.IskalievaLE.WebPl.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private readonly IInventoryLogic _inventoryLogic;
+        // GET: Inventory
+
+
+        public InventoryController()
+        {
+            _inventoryLogic = DependencyResolver.InventoryLogic;
+        }
+        public ActionResult Index()
+        {
+            var inventary = _inventoryLogic.GetAllInventory();
+            return View(inventary);
+        }
+    }
+}
